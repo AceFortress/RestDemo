e0496190fe160e4b92353f8c72f1994d96145747
AceFortress <AceFortress@users.noreply.github.com>
2018-03-24 19:04:07 +0100

    allow cors

--- src/main/java/com/example/demo/CampaignRepo.java
+++ src/main/java/com/example/demo/CampaignRepo.java
@@ -2,7 +2,10 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin
 @RepositoryRestResource(path = "camp")
 public interface CampaignRepo extends CrudRepository<Campaign,Long>{
 }
--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,18 +1,22 @@
 package com.example.demo;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("/**");
-
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
     }
 }
